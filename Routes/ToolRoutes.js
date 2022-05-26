3b74c9553b99219ed779565036480f7e7333b20d
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-26 19:00:57 +0600

    product delete api added

--- Routes/ToolRoutes.js
+++ Routes/ToolRoutes.js
@@ -24,6 +24,17 @@ app.get("/api/fetch-tools", async (req, res) => {
         res.status(500).send(error);
     }
 });
+app.get("/api/fetch-tools-delete", async (req, res) => {
+    const tools = await toolModel
+        .find({})
+        .select(["name", "quantity", "price", "min"]);
+
+    try {
+        res.send(tools);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 app.get("/api/fetch-single-tool/:_id", async (req, res) => {
     console.log(req.params._id);
@@ -35,6 +46,19 @@ app.get("/api/fetch-single-tool/:_id", async (req, res) => {
         res.send(tool);
     } catch (e) {
         res.status(500).send(e);
+    }
+});
+
+app.delete("/api/delete-product/:_id", async (req, res) => {
+    const query = { _id: ObjectId(req.params._id) };
+    try {
+        const product = await toolModel.findByIdAndDelete(query);
+        console.log(product);
+
+        if (!product) res.status(404).send("No item found");
+        res.status(200).send();
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
