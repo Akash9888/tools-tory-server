b2a9f4be243f35f523e6561e6eaebe535c981098
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-27 13:08:18 +0600

    port change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 connectToMongo();
 
